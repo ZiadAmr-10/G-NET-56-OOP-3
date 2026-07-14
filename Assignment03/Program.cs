6c9f139f472b55a64676d4031cc2d662919ec5ed
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-14 15:12:40 +0300

    Question01

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            #region Q01
+            /*
+             a) A University has Departments. If the university is closed, the departments no longer exist.
+                Compostition ->Tigh coupling
+             b) A Driver uses a Car. The driver does not own the car.
+                 Association
+             c) A Dog is an Animal.
+                inheritance
+             d) A Team has Players. If the team is deleted, the players still exist.
+                Aggregation ->less coupling
+             e) A method receives a Logger as a parameter and calls it inside the method only.
+                Dependancy
+             */
+            #endregion
+            #endregion
         }
     }
 }
