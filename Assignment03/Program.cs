7438ee97d28e78196c16e0e0b3dc41141174930c
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-07-14 16:05:08 +0300

    Question02

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -19,6 +19,17 @@
                 Dependancy
              */
             #endregion
+            #region Q02
+            /*
+             a) yes,child can access a protected field in a different assembly,
+                but the object ant access it because protect member behave like private
+             b) Private protected : inheret as protected private but use private with only the project with the same assembly
+                internal protected : inheret as : internal protected but use as internal cannot access outside the assembly
+             c) sealed keyword use to prevents the inheritance for the next class and the sealed class cann't be inherited
+                A sealed method prevents overriding of that method
+             d)yes,because using seald only to prevents inheritance
+             */
+            #endregion
             #endregion
         }
     }
